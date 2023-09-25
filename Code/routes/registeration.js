f335e743fc03681771bb84e17c5e3f8347d1ea2e
lukhanyoVena808 <loookmane001@gmail.com>
2023-09-25 04:10:50 +0200

    Email OTP Update

--- Code/routes/registeration.js
+++ Code/routes/registeration.js
@@ -23,14 +23,26 @@ router.get('/register', function(req, res, next) {
 
 
 // email validation
-const email200 = new Email({
-  message: {
-    from: 'lxgog@mail.com'
-  },
-  // uncomment below to send emails in development/test env:
-  // send: true
-  transport: {
-    jsonTransport: true
+// const email200 = new Email({
+//   message: {
+//     from: 'lxgog@mail.com'
+//   },
+//   // uncomment below to send emails in development/test env:
+//   // send: true
+//   transport: {
+//     jsonTransport: true
+//   }
+// });
+
+const Mailgen = require('mailgen');
+
+const nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "0a870ddc03757e",
+    pass: "0f32dfe8d840ca"
   }
 });
 //
@@ -67,19 +79,60 @@ router.post('/registration', urlencodedParser, async function(req, res, next) {
               }
               console.log(otp);
           
-              email200.send({
-                  template: './mars',
-                  message: {
-                    to: email
-                  },
-                  locals: {
-                    name: name,
-                    _otp: otp
-                  }
-                })
+              // email200.send({
+              //     template: './mars',
+              //     message: {
+              //       to: email
+              //     },
+              //     locals: {
+              //       name: name,
+              //       _otp: otp
+              //     }
+              //   })
                 // .then(console.log)
-                .catch(console.error)
-                        
+                // .catch(console.error)
+                // verify connection configuration
+                const mailGenerator = new Mailgen({
+                  theme: 'default',
+                  product: {
+                      // Appears in header & footer of e-mails
+                      name: 'Mailgen',
+                      link: 'https://mailgen.js/'
+                      // Optional product logo
+                      // logo: 'https://mailgen.js/img/logo.png'
+                  }
+                });
+                const email300 = {
+                  body : {
+                      name: name,
+                      intro : 'Welcome to Test Mail! We\'re very excited to have you on board: ',
+                      outro: 'Need help, or have questions? Just reply to this email, we\'d love to help.'
+                  }
+              }
+              try {
+                // Generate an HTML email with the provided contents
+                // var emailBody = mailGenerator.generate(email300);y
+                const mailOptions = {
+                  from: 'lxgog@mail.com',
+                  to: email,
+                  subject: 'E-Votez OTP Code',
+                  html: "<h1>"+otp+"</h1>"
+              };
+              await transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log(error);
+                    res.status(500).send('Error sending email');
+                } else {
+                    console.log('Email sent: ' + info.response);
+                    res.send('Email sent successfully');
+                }
+             });
+                                
+              } catch (error) {
+                console.log(error)
+              }
+
+        
             res.render('register2', {encrypted_word1: name, encrypted_word2: surname, encrypted_word3: personID, encrypted_word4:email, encrypted_word5:otp});
             
 });
